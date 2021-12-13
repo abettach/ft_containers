5219b90ccca768ddd92b0fef7bef70d1d77c0abf
Achraf Bettachi <bettachiachraf@gmail.com>
2021-12-13 11:07:39 +0100

    iterator

--- test.cpp
+++ test.cpp
@@ -2,16 +2,35 @@
 #include <vector>
 int     main()
 {
-    std::vector<int> vec;
-    vec.push_back(5);
-    vec.push_back(4);
-    vec.push_back(9);
-    vec.push_back(8);
+    std::vector <int> test;
 
-    for (size_t i = 0; i < vec.size(); i++)
-        std::cout << vec[i] << std::endl;
-    vec.resize(2,8);
-    std::cout << "After Resize" << std::endl;
-    for (size_t i = 0; i < vec.size(); i++)
-        std::cout << vec[i] << std::endl;
+    test.push_back(9);
+    test.push_back(1);
+    test.push_back(2);
+    test.push_back(3);
+    test.push_back(4);
+    test.push_back(5);
+
+    try
+    {
+    test.at(6);
+        
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    // std::vector<int> vec;
+    // vec.push_back(5);
+    // vec.push_back(4);
+    // vec.push_back(9);
+    // vec.push_back(8);
+
+    // for (size_t i = 0; i < vec.size(); i++)
+    //     std::cout << vec[i] << std::endl;
+    // vec.resize(2,8);
+    // std::cout << "After Resize" << std::endl;
+    // for (size_t i = 0; i < vec.size(); i++)
+    //     std::cout << vec[i] << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -5,44 +5,86 @@
 // #include <vector>
 namespace ft
 {
-    template < class T, class Alloc = std::allocator<T> > class vector
+    template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
+    struct iterator
+    {
+        typedef T         value_type;
+        typedef Distance  difference_type;
+        typedef Pointer   pointer;
+        typedef Reference reference;
+        typedef Category  iterator_category;
+    };
+    template< class Iterator >
+    struct iterator_traits
+    {
+		typedef typename Iterator::difference_type difference_type;
+		typedef typename Iterator::value_type value_type;
+		typedef typename Iterator::pointer pointer;
+		typedef typename Iterator::reference reference;
+		typedef typename Iterator::iterator_category iterator_category;
+    };
+    template< class T >
+    struct iterator_traits<T*>
+    {
+		typedef ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef T *pointer;
+		typedef T &reference;
+		typedef typename std::random_access_iterator_tag iterator_category;
+    };
+    template< class T >
+    struct iterator_traits<const T*>
+    {
+        typedef ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef const T *pointer;
+		typedef const T &reference;
+		typedef typename std::random_access_iterator_tag iterator_category;
+    };
+
+    template < class T >
+    class MyIterator : public std::iterator<std::random_access_iterator_tag, typename iterator_traits<T>::value_type>
+    {
+        public:
+
+    };
+    template < class T, class Alloc = std::allocator<T> >
+    class vector
     {
     private:
         typedef T value_type;
         typedef Alloc allocator_type;
         typedef typename allocator_type::reference reference;
-        typedef typename allocator_type::const_reference	 const_reference;
-        typedef typename allocator_type::pointer	pointer;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
         typedef typename allocator_type::const_pointer const_pointer;
         typedef size_t size_type;
     private:
-       value_type *arr;
+       allocator_type _alloc;
+       value_type *_arr;
        size_type _size;
        size_type _capacity;
-       Alloc _alloc;
     public:
-        vector()
-        {
-            arr = new T[1];
-            _size = 0;
-            _capacity = 1;
-        }
+		vector(const allocator_type &alloc = allocator_type()) : _alloc(alloc), _arr(nullptr), _size(0), _capacity(0)
+		{
+		}
+
+		vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type()) : _alloc(alloc), _arr(nullptr), _size(n), _capacity(n)
+		{
+			_arr = _alloc.allocate(n);
+			for (unsigned long i = 0; i < n; i++)
+				_arr[i] = val;
+		}
         void push_back (const value_type& val)
         {
             if (_size == _capacity)
-            {
-                T *tmp = new T[2 * _capacity];
-                for (size_t i = 0; i < _capacity; i++)
-                    tmp[i] = arr[i];
-                delete[] arr;
-                arr = tmp;
-                _capacity *= 2;
-            }
-            arr[_size] = val;
+                resize(_capacity);
+            _arr[_size] = val;
             _size++;
         }
         void    pop_back()
         {
+            _alloc.destroy(&_arr[_size - 1]);
             _size--;
         }
         size_type size() const
@@ -53,15 +95,66 @@ namespace ft
         {
             return (this->_capacity);
         }
+        void resize (size_type n, value_type val = value_type())
+        {
+            T *new_arr;
+            if (n > this->_capacity)
+            {
+                _capacity *= 2;
+                _capacity = _capacity == 0 ? 1 : _capacity;
+                _capacity = n > _capacity ? n : _capacity;
+            }
+            _size = _size > n ? n : _size;
+            new_arr = _alloc.allocate(_capacity);
+            for (size_t i = 0; i < _size; i++)
+                new_arr[i] = _arr[i];
+            while (_size++ < n)
+                new_arr[_size] = val;
+            delete[] _arr;
+            _arr = new_arr;
+        }
         size_type max_size() const
         {
-            return ();
+            return (_alloc.max_size());
         }
-        T   operator[](int i)
+        reference operator[] (size_type n)
         {
-            return arr[i];
+            return (this->_arr[n]);
         }
-        class iterator : 
+        const_reference operator[] (size_type n) const
+        {
+            return (_arr[n]);
+        }
+        reference at (size_type n)
+        {
+            if (n < 0 || n > _size)
+                throw std::out_of_range("index out of bounds");
+            else
+                return (this->_arr[n]);
+        }
+        const_reference at (size_type n) const
+        {
+            if (n < 0 || n > _size)
+                throw std::out_of_range("index out of bounds");
+            else
+                return (this->_arr[n]);
+        }
+        reference front()
+        {
+            return (this->_arr[0]);
+        }
+        const_reference front() const
+        {
+            return (this->_arr[0]);
+        }
+        reference back()
+        {
+            return (this->_arr[this->_size - 1]);
+        }
+        const_reference back() const
+        {
+            return (this->_arr[this->_size - 1]);
+        }
     };
 }
 
